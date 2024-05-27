9d038ed934587694611f5b964d40342e3a2aad84
Alexander Schmiedel <alexander.schmiedel@gmail.com>
2024-05-27 09:52:58 +0200

    Added Sobel Filter to ResNet

--- deepcluster/models/ResNet.py
+++ deepcluster/models/ResNet.py
@@ -69,7 +69,7 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, n_blocks: iter, img_channels: int = 3, num_classes: int = 1000):
+    def __init__(self, block, n_blocks: iter, img_channels: int = 3, num_classes: int = 1000, sobel = False):
         super(ResNet, self).__init__()
         self.in_channels: int = 64
 
@@ -85,6 +85,18 @@ class ResNet(nn.Module):
 
         self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
         self.top_layer = nn.Linear(in_features=512, out_features=num_classes)
+        
+        if sobel:
+            grayscale = nn.Conv2d(in_channels=3, out_channels=1, kernel_size=1, stride=1, padding=0)
+            grayscale.weight.data.fill_(1.0 / 3.0)
+            grayscale.bias.data.zero_()
+            filter = nn.Conv2d(in_channels=1, out_channels=2, kernel_size=3, stride=1, padding=1)
+            filter.weight.data[0, 0].copy_(torch.FloatTensor([[1, 0, -1], [2, 0, -2], [1, 0, -1]]))
+            filter.weight.data[1, 0].copy_(torch.FloatTensor([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]))
+            filter.bias.data.zero_()
+            self.sobel = nn.Sequential(grayscale, filter)
+            for parameter in self.sobel.parameters():
+                parameter.require_grad = False
 
     def _make_layer(self, block, out_channels: int, n_blocks: int, stride=1) -> nn.Module:
         layers = []
@@ -108,6 +120,8 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x: Tensor) -> Tensor:
+        if self.sobel:
+            out = self.sobel(out)
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
@@ -125,21 +139,21 @@ class ResNet(nn.Module):
         return out
 
 
-def resnet18():
-    return ResNet(BasicBlock, (2, 2, 2, 2))
+def resnet18(sobel=False):
+    return ResNet(BasicBlock, (2, 2, 2, 2, sobel))
 
 
-def resnet34():
-    return ResNet(BasicBlock, (3, 4, 6, 3))
+def resnet34(sobel=False):
+    return ResNet(BasicBlock, (3, 4, 6, 3, sobel))
 
 
-def resnet50():
-    return ResNet(Bottleneck, (3, 4, 6, 3))
+def resnet50(sobel=False):
+    return ResNet(Bottleneck, (3, 4, 6, 3, sobel))
 
 
-def resnet101():
-    return ResNet(Bottleneck, (3, 4, 23, 3))
+def resnet101(sobel=False):
+    return ResNet(Bottleneck, (3, 4, 23, 3, sobel))
 
 
-def resnet152():
-    return ResNet(Bottleneck, (3, 8, 36, 3))
+def resnet152(sobel=False):
+    return ResNet(Bottleneck, (3, 8, 36, 3, sobel))
