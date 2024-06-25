182d2d37beb725017d75dc2a78d04a66a3896369
Alexander Schmiedel <alexander.schmiedel@gmail.com>
2024-06-25 23:14:07 +0200

    Fix: Cluster logs with wrong format for original implementation

--- deepcluster/deepcluster.py
+++ deepcluster/deepcluster.py
@@ -297,7 +297,10 @@ class DeepCluster(BaseEstimator):
             self.print_results(epoch, losses, accuracies, train_data.dataset.targets, target_labels)
 
             # Store psuedo-labels
-            self.cluster_logs.append(train_data.dataset.targets)
+            if self.clustering_method == 'faiss_orig':
+                self.cluster_logs.append(self.clustering.images_lists)
+            else:
+                self.cluster_logs.append(train_data.dataset.targets)
 
             self.execution_time.append(time.time() - start_time)
 
