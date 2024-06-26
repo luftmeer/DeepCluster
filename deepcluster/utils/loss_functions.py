df3b689a0d5dc2322481782a08b767e33f1559c4
FloKit <f.kittel@gmx.net>
2024-06-26 21:00:20 +0200

    add contrastive loss function loader

--- deepcluster/utils/loss_functions.py
+++ deepcluster/utils/loss_functions.py
@@ -2,29 +2,7 @@ from torch import nn
 import torch
 
 LOSS_FUNCTIONS = ['L1', 'L2', 'MSE', 'CrossEntropy']
-
-def loss_function_loader(loss_fn: str):
-    """Simple Wrapper to load a Loss Function of a pre-defined selection.
-
-    Parameter
-    ---------
-    loss_fn: str,
-        Name of the loss function.
-        
-    Return
-    -----
-    Loss Function object.
-    """
-    
-    if loss_fn not in LOSS_FUNCTIONS:
-        raise ValueError(f'Selected loss function {loss_fn} not supported.')
-    
-    if loss_fn == 'L1':
-        return nn.L1Loss()
-    elif loss_fn in ['L2', 'MSE']:
-        return nn.MSELoss()
-    elif loss_fn == 'CrossEntropy':
-        return nn.CrossEntropyLoss()
+CONTRASTIVE_LOSS_FUNCTIONS = ['Contrastive', 'NT_Xent']
     
 class ContrastiveLoss(nn.Module):
     def __init__(self, temperature=0.1):
@@ -78,3 +56,47 @@ class NT_XentLoss(torch.nn.Module):
         loss /= N
         
         return loss
+    
+def loss_function_loader(loss_fn: str):
+    """Simple Wrapper to load a Loss Function of a pre-defined selection.
+
+    Parameter
+    ---------
+    loss_fn: str,
+        Name of the loss function.
+        
+    Return
+    -----
+    Loss Function object.
+    """
+    
+    if loss_fn not in LOSS_FUNCTIONS:
+        raise ValueError(f'Selected loss function {loss_fn} not supported.')
+    
+    if loss_fn == 'L1':
+        return nn.L1Loss()
+    elif loss_fn in ['L2', 'MSE']:
+        return nn.MSELoss()
+    elif loss_fn == 'CrossEntropy':
+        return nn.CrossEntropyLoss()
+    
+def contrastive_loss_function_loader(loss_fn: str):
+    """Simple Wrapper to load a Contrastive Loss Function of a pre-defined selection.
+
+    Parameter
+    ---------
+    loss_fn: str,
+        Name of the loss function.
+        
+    Return
+    -----
+    Loss Function object.
+    """
+    
+    if loss_fn not in ['Contrastive', 'NT_Xent']:
+        raise ValueError(f'Selected loss function {loss_fn} not supported.')
+    
+    if loss_fn == 'Contrastive':
+        return ContrastiveLoss()
+    elif loss_fn == 'NT_Xent':
+        return NT_XentLoss()
